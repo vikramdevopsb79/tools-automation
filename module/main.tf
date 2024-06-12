95c9a21871f34e1e3eb7aeca7466c9c1858f9f0f
sai <kuppalasaivikram@gmail.com>
2024-06-12 11:45:57 +0530

    added port details for vault

--- module/main.tf
+++ module/main.tf
@@ -30,6 +30,15 @@ resource "aws_instance" "node" {
   ami           = data.aws_ami.ami.id
   instance_type = var.instance_type
   vpc_security_group_ids = [data.aws_security_group.allow-all.id]
+  instance_market_options {
+    market_type = "spot"
+    spot_options {
+      instance_interruption_behavior  = "stop"
+      spot_instance_type              = "persistent"
+    }
+  }
+
+
 
   tags = {
     Name = var.name
