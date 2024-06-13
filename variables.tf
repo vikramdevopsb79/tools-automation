e47d4a5f2e41ef7b85052674b4fe7b53a5f3d603
sai <kuppalasaivikram@gmail.com>
2024-06-13 14:54:23 +0530

    prometheus installation

--- variables.tf
+++ variables.tf
@@ -5,5 +5,10 @@ variable "tools" {
       instance_type = "t3.small"
       port_no       = "8200"
     }
+    prometheus = {
+      name          = "prometheus"
+      instance_type = "t3.small"
+      port_no       = "9090"
+    }
   }
 }
